6552bbc81cfa0241cdabe94f000c4f104da47603
Kristen M <kristenmayorga@gmail.com>
2020-09-20 18:37:53 -0400

    added startgame function and newgame function

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 console.log(choices);
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -54,8 +54,57 @@ let questions = [
 ];
 
 
-var MAX_QUESTIONS = 5;
-var SUBTRACT_SCORE = 10;
+const MAX_QUESTIONS = 5;
+const SUBTRACT_SCORE = 10;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+getNewQuestion();
+
+};
 
 
 
+getNewQuestion = () => {
+
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS ) {
+    return window.location.assign("/end.html");
+  }
+
+  questionCounter++;
+  
+
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+   
+      getNewQuestion();
+ 
+  });
+});
+
+
+
+startGame();
